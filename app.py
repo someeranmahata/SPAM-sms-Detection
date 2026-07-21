484b8b9e3d9eaf5d12b013c7fbce83077475b98e
someeranmahata <someeranmahata@gmail.com>
2026-07-21 19:36:37 +0530

    flask code is added

--- app.py
+++ app.py
@@ -64,23 +64,44 @@ print(type(vector))
 #random 5 spam and 5 ham message from stemming data
 data = {'ringtoneking 84484': 1, 'Thanks for the Vote. Now sing along with the stars with Karaoke on your mobile. For a FREE link just reply with SING now.': 1, 'Want a new Video Phone? 750 anytime any network mins? Half price line rental free text for 3 months? Reply or call 08000930705 for free delivery': 1, 'Mobile Club: Choose any of the top quality items for your mobile. 7cfca1a': 1, 'Spook up your mob with a Halloween collection of a logo & pic message plus a free eerie tone, txt CARD SPOOK to 8007 zed 08701417012150p per logo/pic ': 1, 'Hey whats up? U sleeping all morning?': 0, "Sorry, got a late start, we're on the way": 0, 'I wish that I was with you. Holding you tightly. Making you see how important you are. How much you mean to me ... How much I need you ... In my life ...': 0, 'Thank You for calling.Forgot to say Happy Onam to you Sirji.I am fine here and remembered you when i met an insurance person.Meet You in Qatar Insha Allah.Rakhesh, ex Tata AIG who joined TISSCO,Tayseer.': 0, 'Japanese Proverb: If one Can do it, U too Can do it, If none Can do it,U must do it Indian version: If one Can do it, LET HIM DO it.. If none Can do it,LEAVE it!! And finally Kerala version: If one can do it, Stop him doing it.. If none can do it, Make a strike against it ...': 0}
 
-for key in data:
-    processed = convert(key)
-    X = vect(processed)
+# for key in data:
+#     processed = convert(key)
+#     X = vect(processed)
 
     
-    pred = model.predict(X)[0]
+#     pred = model.predict(X)[0]
 
-    print(f"Actual: {data[key]}, Predicted: {pred}")
+#     print(f"Actual: {data[key]}, Predicted: {pred}")
 
 
 
-# app = Flask(__name__)
+app = Flask(__name__)
 
-# @app.route("/", methods=["GET", "POST"])
-# def index():
-#     return render_template("index.html")
+@app.route("/", methods=["GET", "POST"])
+def index():
+    status = None
+    para = False
+    
+    if(request.method == 'POST'):
+        text = request.form.get('user_input')
+        para = True
+        
+        print(text)
+        
+        processed = convert(text)
+        vt = vect(processed)
 
     
-# if __name__ == "__main__":
-#     app.run(debug=True)
+        pred = model.predict(vt)[0]
+        status = pred
+        
+        
+    return render_template(
+        "index.html",
+        para = para,
+        status = status
+        )
+
+    
+if __name__ == "__main__":
+    app.run(debug=True)
